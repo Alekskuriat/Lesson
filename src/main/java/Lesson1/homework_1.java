0583141a7f725a1ab20cdca62a10c86f2e6fa3ac
Aleks <kuriatae@mail.ru>
2021-02-03 16:15:57 +0700

    create main

---
+++ src/main/java/Lesson1/homework_1.java
@@ -0,0 +1,16 @@
+package Lesson1;
+
+public class homework_1 {
+
+    public static void main(String[] args){
+        initVar();
+
+    }
+
+    private static void initVar() {
+        int varInt = 5;
+
+    }
+
+
+}
